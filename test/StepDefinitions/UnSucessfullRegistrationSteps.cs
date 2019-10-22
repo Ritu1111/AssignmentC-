986a6d39cc2273be95829a40e6363bf8818bf3a9
Ritu1111 <54574049+Ritu1111@users.noreply.github.com>
2019-10-22 10:14:17 +0530

    Update UnSucessfullRegistrationSteps.cs

--- test/StepDefinitions/UnSucessfullRegistrationSteps.cs
+++ test/StepDefinitions/UnSucessfullRegistrationSteps.cs
@@ -1,33 +1,40 @@
-﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
 using TechTalk.SpecFlow;
 
-namespace test.StepDefinitions
+namespace test.StepDefinations
 {
     [Binding]
     public class UnSucessfullRegistrationSteps
     {
+        RestClient client;
+        RestRequest request;
         [Given(@"We have client and request")]
         public void GivenWeHaveClientAndRequest()
         {
-            ScenarioContext.Current.Pending();
+            client = new RestClient("https://reqres.in/.");
+            request = new RestRequest("/api/login", Method.POST);
         }
-        
+
         [Given(@"Pass the Parameter")]
         public void GivenPassTheParameter()
         {
-            ScenarioContext.Current.Pending();
+            request.AddParameter("email", "sydney@fife");
         }
-        
+
         [When(@"Response  is collected")]
         public void WhenResponseIsCollected()
         {
-            ScenarioContext.Current.Pending();
+            IRestResponse response = client.Execute(request);
+            var content = response.Content;
+            Console.WriteLine(content);
+            Deser deserializedProduct = JsonConvert.DeserializeObject<Deser>(content);
+            Assert.AreEqual(deserializedProduct.id, 0);
+            Assert.AreEqual(deserializedProduct.token, "QpwL5tke4Pnpja7X4");
+            Console.ReadLine();
         }
-        
-        [Then(@"token is generated")]
-        public void ThenTokenIsGenerated()
-        {
-            ScenarioContext.Current.Pending();
-        }
+
     }
 }
